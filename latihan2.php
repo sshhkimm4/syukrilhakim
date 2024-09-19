c7b0709cb60b5451bc13f612ef94d70d53547174
syukrilhakim.png <syukrilhakim30@gmail.com>
2024-09-20 01:49:18 +0700

    Add files via upload

---
+++ latihan2.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$nrp = "12345678";
+$nama = "anutu";
+$umur = 100;
+$nilai = 95.95;
+
+echo "Nrp  : ". $nrp . "</br>";
+echo "Nama  : $nama</br>";
+print "Umur : ".$umur; print "</br>";
+
+// menampilkan bilangan real dengan format 3 digit belakang koma
+printf("Nilai : %.3f</br>", $nilai);
+
+?>
